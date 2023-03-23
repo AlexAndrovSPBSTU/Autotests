41f6428f92002d44ea3c6908c9d258d31962e1ff
Deezorch <deezo5147@gmail.com>
2023-03-23 21:20:46 +0300

    initial

--- src/main/java/org/example/ProfilePage.java
+++ src/main/java/org/example/ProfilePage.java
@@ -1,10 +1,20 @@
 package org.example;
 
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 
+import java.util.List;
+
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class ProfilePage {
+    protected static List<SelenideElement> sideElements = $$(By.className("nav-side_i-w"));
+
+    public static List<SelenideElement> getSideElements() {
+        return sideElements;
+    }
+
     private final SelenideElement sideMenu = $x("//*[@id=\"hook_Block_Navigation\"]/div/div/div[1]/a/div");
     private final SelenideElement logoutBtn = $x("//*[@id=\"hook_Block_ToolbarUserDropdown\"]/div/div[2]/div/div[1]/div/div[1]/div[2]/a");
     private final SelenideElement userMenu = $x("//*[@id=\"hook_Block_ToolbarUserDropdown\"]/div/div[1]");
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -6,13 +6,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+
 public class LoginTest {
     public static LoginPage loginPage;
     public static ProfilePage profilePage;
 
     @BeforeAll
     public static void setup() {
-        loginPage = new LoginPage(ConfProperties.getProperty("url"));
+        loginPage = new LoginPage("https://ok.ru/");
         profilePage = new ProfilePage();
     }
 
@@ -24,6 +28,15 @@ public class LoginTest {
         String user = profilePage.getUserName();
         String expected = ConfProperties.getProperty("login");
         Assertions.assertEquals(expected, user);
+    }
+
+    //Mather exercise
+    @Test
+    public void checkItemsQuantity() {
+        loginPage.inputLogin("botS23AT1");
+        loginPage.inputPasswd("autotests2023");
+        loginPage.clickLoginBtn();
+        assertThat(ProfilePage.getSideElements().size(), equalTo(9));
     }
 
     @AfterAll
